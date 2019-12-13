3a41a39bc5b931791df74011dcc2030832497079
unknown <viola4lfe@gmail.com>
2019-12-13 11:47:18 -0500

    Name, Height, Gender rendering to page: before adding card from reactstrap

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+
 import "./App.css";
 import axios from "axios";
 import PeopleCard from "./components/PeopleCard";
-import PeopleCard from "./components/PeopleCard";
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -14,20 +15,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`http https://swapi.co/api/people/`)
-  //     .then(response => {
-  //       console.log(response);
-  //     })
-  //     .catch(error => console.log("Sorry no people", error));
-  // }, []);
-
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => {
+        console.log(response.data.results);
+        setPeople(response.data.results)
+      })
+      .catch(error => console.log("Sorry no people", error));
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-      <PeopleCard />
+      <PeopleCard people={people} />
     </div>
   );
 };
--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 const PeopleCard = props => {
+  const { people } = props;
 
+  return (
+    <>
+      {people.map(props => {
+        console.log(props);
 
- return(
-  <>
-
-  </>
- )
+        return (
+          <>
+            <h1>Name: {props.name} </h1>
+            <h2>Height: {props.height}</h2>
+            <h3>Gender: {props.gender}</h3>
+          </>
+        );
+      })}
+    </>
+  );
 };
 
 export default PeopleCard;
