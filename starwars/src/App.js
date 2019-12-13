812ffdd3a823d2bb8d4420786607e9a110843549
unknown <viola4lfe@gmail.com>
2019-12-13 11:15:43 -0500

    initial second component setup

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
+import PeopleCard from "./components/PeopleCard";
+import PeopleCard from "./components/PeopleCard";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -12,9 +14,20 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // useEffect(() => {
+  //   axios
+  //     .get(`http https://swapi.co/api/people/`)
+  //     .then(response => {
+  //       console.log(response);
+  //     })
+  //     .catch(error => console.log("Sorry no people", error));
+  // }, []);
+
+
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <PeopleCard />
     </div>
   );
 };
---
+++ starwars/src/components/PeopleCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PeopleCard = props => {
+
+
+ return(
+  <>
+
+  </>
+ )
+};
+
+export default PeopleCard;
