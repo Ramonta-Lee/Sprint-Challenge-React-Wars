ac6c55e65668c4132770fa378992e86923e47e9f
unknown <viola4lfe@gmail.com>
2019-12-13 12:14:01 -0500

    styling the cards using reactstrap and styled components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import axios from "axios";
 import PeopleCard from "./components/PeopleCard";
 
-
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -15,15 +14,15 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  useEffect(() => {
-    axios
-      .get("https://swapi.co/api/people/")
-      .then(response => {
-        console.log(response.data.results);
-        setPeople(response.data.results)
-      })
-      .catch(error => console.log("Sorry no people", error));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("https://swapi.co/api/people/")
+  //     .then(response => {
+  //       console.log(response.data.results);
+  //       setPeople(response.data.results)
+  //     })
+  //     .catch(error => console.log("Sorry no people", error));
+  // }, []);
 
   return (
     <div className="App">
--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,4 +1,14 @@
 import React from "react";
+import styled from "styled-components";
+import { Card, Button, CardTitle, CardText } from "reactstrap";
+
+const StyledCard = styled.div`
+ width: 50%;
+ display: flex;
+ flex-direction: column;
+ justify-content: center; 
+
+`;
 
 const PeopleCard = props => {
   const { people } = props;
@@ -9,11 +19,25 @@ const PeopleCard = props => {
         console.log(props);
 
         return (
-          <>
-            <h1>Name: {props.name} </h1>
+          <div>
+            <StyledCard
+              body
+              inverse
+              style={{ backgroundColor: "#333", borderColor: "#333" }}
+              width={{width: "30px"}}
+            >
+              <CardTitle>Name: {props.name}</CardTitle>
+              <CardText>
+                Height: {props.height}
+                Gender: {props.gender}
+              </CardText>
+              <Button>Button</Button>
+            </StyledCard>
+
+            {/* <h1>Name: {props.name} </h1>
             <h2>Height: {props.height}</h2>
-            <h3>Gender: {props.gender}</h3>
-          </>
+            <h3>Gender: {props.gender}</h3> */}
+          </div>
         );
       })}
     </>
