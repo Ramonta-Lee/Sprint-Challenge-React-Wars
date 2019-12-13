bb391507c0c136f7e32114281db5c172e4598d95
unknown <viola4lfe@gmail.com>
2019-12-13 13:00:05 -0500

    start of Pagination Stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import PeopleCard from "./components/PeopleCard";
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -19,14 +20,33 @@ const App = () => {
       .get("https://swapi.co/api/people/")
       .then(response => {
         // console.log(response.data.results);
-        setPeople(response.data.results)
+        setPeople(response.data.results);
       })
       .catch(error => console.log("Sorry no people", error));
   }, []);
- 
 
   return (
     <div className="App">
+      <Pagination aria-label="Page navigation example">
+        <PaginationItem>
+          <PaginationLink first href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink previous href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink href="#">2</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink next href="#" />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink last href="#" />
+        </PaginationItem>
+      </Pagination>
       <h1 className="Header">React Wars</h1>
       <PeopleCard people={people} />
     </div>
